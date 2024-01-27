d98a0e1d6ce851f7fe8b746b27dc500a70e05685
susu <kwdfw@outlook.com>
2024-01-27 21:33:25 +0800

    Data RW Process

--- bitcask-go/fio/io_manager.go
+++ bitcask-go/fio/io_manager.go
@@ -4,7 +4,7 @@ const DataFilePerm = 0644
 
 // IOManger 抽象 IO 管理接口，可以接入不同的 IO 类型，目前支持标准文件 IO
 
-type IOManger interface {
+type IOManager interface {
 	// Read 从文件的给定位置读取对应的数据
 	Read([]byte, int64) (int, error)
 
