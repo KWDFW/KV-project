10919da889d20c11f9caf5b7163355d297499c17
susu <kwdfw@outlook.com>
2024-02-09 07:52:47 +0800

    Codecs

--- bitcask-go/data/log_record.go
+++ bitcask-go/data/log_record.go
@@ -1,6 +1,9 @@
 package data
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"hash/crc32"
+)
 
 type LogRecordType = byte
 
@@ -36,15 +39,69 @@ type LogRecordPos struct {
 }
 
 // EncodeLogRecord 对LogRecord进行编码，返回字节数组及长度
+// crc校验值		|type类型		|key size		|value size		|key		|value
+// 4字节         1 字节 		变长(max 5) 变长(max 5)		变长     变长
 func EncodeLogRecord(LogRecord *LogRecord) ([]byte, int64) {
-	return nil, 0
+	// 初始化一个header部分的字节数组
+	header := make([]byte, maxLogRecordHeaderSize)
+
+	// 第五个字节存储 Type
+	header[4] = LogRecord.Type
+	var index = 5
+	// 5字节之后，存储的是key和value的长度信息
+	// 使用变长类型，节省空间
+	index += binary.PutVarint(header[index:], int64(len(LogRecord.Key)))
+	index += binary.PutVarint(header[index:], int64(len(LogRecord.Value)))
+
+	var size = index + len(LogRecord.Key) + len(LogRecord.Value)
+	encBytes := make([]byte, size)
+
+	// 将header部分的内容拷贝过来
+	copy(encBytes[:index], header[:index])
+	// 将key和value数据拷贝到字节数组中
+	copy(encBytes[index:], LogRecord.Key)
+	copy(encBytes[index+len(LogRecord.Key):], LogRecord.Value)
+
+	// 对整个LogRecord的数据进行crc校验
+	crc := crc32.ChecksumIEEE(encBytes[4:])
+	binary.LittleEndian.PutUint32(encBytes[:4], crc)
+
+	return encBytes, int64(size)
 }
 
 // 对字节数组中的Header信息进行解码
 func decodeLogRecordHeader(buf []byte) (*logRecordHeader, int64) {
-	return nil, 0
+	if len(buf) <= 4 {
+		return nil, 0
+	}
+
+	header := &logRecordHeader{
+		crc:        binary.LittleEndian.Uint32(buf[:4]),
+		recordType: buf[4],
+	}
+
+	var index = 5
+	// 取出实际的key size
+	keySize, n := binary.Varint(buf[index:])
+	header.keySize = uint32(keySize)
+	index += n
+
+	// 取出实际的value size
+	valueSize, n := binary.Varint(buf[index:])
+	header.valueSize = uint32(valueSize)
+	index += n
+
+	return header, int64(index)
 }
 
 func getLogRecordCRC(lr *LogRecord, header []byte) uint32 {
-	return 0
+	if lr == nil {
+		return 0
+	}
+
+	crc := crc32.ChecksumIEEE(header[:])
+	crc = crc32.Update(crc, crc32.IEEETable, lr.Key)
+	crc = crc32.Update(crc, crc32.IEEETable, lr.Value)
+
+	return crc
 }
